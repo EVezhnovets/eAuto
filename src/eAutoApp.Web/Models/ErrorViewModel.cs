8c3029e09afb3a7a17cb69941d95c881a2c19337
Euheni <eugene.vezhnavets@gmail>
2023-02-02 23:44:18 +0300

    Create mvc project

---
+++ src/eAutoApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eAutoApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
