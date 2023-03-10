83c06b0f0d1e2c1f95375ef0192f02c56bbdb879
Euheni <eugene.vezhnavets@gmail>
2023-03-11 00:14:30 +0300

    Add DiConfigurator class, add and register context

---
+++ src/DiConfigurator/DiConfigurator.cs
@@ -0,0 +1,29 @@
+﻿using eAuto.Data.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiConfigurator
+{
+    public sealed class DiConfigurator
+    {
+        private readonly string _connectionString;
+        private readonly IConfiguration _configuration;
+
+        public DiConfigurator(string connectionString, IConfiguration configuration)
+        {
+            _connectionString = connectionString;
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            RegisterDataPart(services);
+        }
+        
+        private void RegisterDataPart(IServiceCollection services)
+        {
+            services.AddTransient(s => new eAutoContext(_connectionString));
+            eAutoContextConfigurator.RegisterContext(services, _connectionString);
+        }
+    }
+}
