63f14a7f2b87a89873065503221091199c4b1b4b
Euheni <eugene.vezhnavets@gmail>
2023-03-19 14:00:35 +0300

    Add BodyTypeViewModel; add create, delete actions in bodyTypeController, fix index action;

--- src/eAuto.Web/Controllers/BodyTypeController.cs
+++ src/eAuto.Web/Controllers/BodyTypeController.cs
@@ -1,24 +1,95 @@
-﻿using eAuto.Domain.Interfaces;
-using eAutoApp.Web.Models;
+﻿using eAuto.Domain.DomainModels;
+using eAuto.Domain.Interfaces;
+using eAuto.Domain.Interfaces.Exceptions;
+using eAuto.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace eAutoApp.Web.Controllers
+namespace eAuto.Web.Controllers
 {
     public class BodyTypeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<BodyTypeController> _logger;
         private readonly IBodyTypeService _bodyTypeService;
 
-        public BodyTypeController(IBodyTypeService bodyTypeService ,ILogger<HomeController> logger)
+        public BodyTypeController(IBodyTypeService bodyTypeService ,ILogger<BodyTypeController> logger)
         {
             _logger = logger;
             _bodyTypeService = bodyTypeService;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
-        {  
-            var result = await _bodyTypeService.GetBodyTypeViewModelsAsync();
-            return View(result);
+        {
+			try
+			{
+				var result = await _bodyTypeService.GetBodyTypeModelsAsync();
+				return View(result);
+			}
+			catch (BodyTypeNotFoundException ex)
+			{
+				//TODO logger
+				return NotFound(ex.Message);
+			}
+			catch(Exception ex)
+			{
+				//TODO logger
+				return BadRequest(ex.Message);
+			}
+            
+        }
+
+		#region Create
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+        [ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(BodyTypeViewModel viewModel)
+		{
+			IBodyType bodyType;
+			try
+			{
+                if (ModelState.IsValid)
+                {
+                    bodyType = await _bodyTypeService.CreateBodyTypeModelAsync(viewModel.Name);
+                    bodyType.Save();
+                    return RedirectToAction("Index");
+                }
+                return View(viewModel);
+            }
+			catch (BodyTypeNotFoundException)
+			{
+				return RedirectToAction("Index");
+			}
+        }
+        #endregion
+
+        #region Delete
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var bodyType = await _bodyTypeService.GetBodyTypeModelAsync(id);
+                bodyType.Delete();
+                return RedirectToAction("Index");
+            }
+
+            catch (BodyTypeNotFoundException ex)
+            {
+                _logger!.LogError(ex.Message);
+                return NotFound(ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+                _logger!.LogError(ex.Message);
+                return BadRequest(ex.Message);
+            }
+            #endregion
         }
     }
 }
--- src/eAuto.Web/Models/ErrorViewModel.cs
+++ src/eAuto.Web/Models/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-namespace eAutoApp.Web.Models
+namespace eAuto.Web.Models
 {
     public class ErrorViewModel
     {
