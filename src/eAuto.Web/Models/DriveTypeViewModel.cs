ccb47ab677a3dd0045b150dc2962a61d815242bb
Euheni <eugene.vezhnavets@gmail>
2023-03-21 23:51:51 +0300

    Add DriveTypeController, DriveTypeViewModel

---
+++ src/eAuto.Web/Models/DriveTypeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace eAuto.Web.Models
+{
+	public sealed class DriveTypeViewModel
+    {
+		public int DriveTypeId { get; set; }
+		[DisplayName("Drive Type")]
+		public string Name { get; set; }
+		public DriveTypeViewModel()
+		{
+		}
+
+		public DriveTypeViewModel(int DriveTypeId, string name)
+		{
+            DriveTypeId = DriveTypeId;
+			Name = name;
+		}
+		//TODO PagedOptions(int skip, int take)
+	}
+}
