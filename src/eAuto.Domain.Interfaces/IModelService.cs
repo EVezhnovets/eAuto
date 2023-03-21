2e27bba5f2d700f5c32335b31e5fb61b8d73cbd5
Euheni <eugene.vezhnavets@gmail>
2023-03-21 13:12:41 +0300

    Add IModelService

---
+++ src/eAuto.Domain.Interfaces/IModelService.cs
@@ -0,0 +1,10 @@
+﻿namespace eAuto.Domain.Interfaces
+{
+    public interface IModelService
+    {
+        IModel GetModelModel(int id);
+        Task<IEnumerable<IModel>> GetModelModelsAsync();
+		IModel CreateModelModel(string name);
+        IModel CreateModelDomainModel();
+    }
+}
