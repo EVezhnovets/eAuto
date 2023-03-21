83b4b50b4db9a71af1579ff7e92f8f7c20f24289
Euheni <eugene.vezhnavets@gmail>
2023-03-21 13:13:55 +0300

    Add OneToOneModel in Web proj, ModelViewModel

---
+++ src/eAuto.Web/Models/OneToOneModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace eAuto.Web.Models
+{
+    public class OneToOneModel<View> where View : class
+    {
+        public View ViewModel { get; set; }
+        public IEnumerable<SelectListItem>? ModelFK { get; set; }
+    }
+}
