34b475c8941b08eb1c8b2a0e343b17913e649acf
Euheni <eugene.vezhnavets@gmail>
2023-03-26 19:46:58 +0300

    Add IImageManager; fix CarDomainModel

---
+++ src/eAuto.Domain.Interfaces/IImageManager.cs
@@ -0,0 +1,9 @@
+﻿namespace eAuto.Domain.Interfaces
+{
+    public interface IImageManager
+    {
+        public List<string> FilesName { get; }
+        public void UploadFiles(Object files, string path);
+        public void RemoveFile(string contentWay, string nameFile);
+    }
+}
