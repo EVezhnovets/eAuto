799b633e642bfbcf1e59ec5c6bebb56ed3562e15
Euheni <eugene.vezhnavets@gmail>
2023-03-26 11:35:15 +0300

    Add Car exceptions, domain model with interface, fix EngineDomainModel

---
+++ src/eAuto.Domain.Interfaces/ICar.cs
@@ -0,0 +1,37 @@
+﻿namespace eAuto.Domain.Interfaces
+{
+    public interface ICar
+    {
+        int CarId { get; set; }
+        decimal PriceInitial { get; set; }
+        string PictureUrl { get; set; }
+        DateTime Year { get; set; }
+        DateTime DateArrival { get; set; }
+        int Odometer { get; set; }
+        public string Description { get; set; }
+
+		int BrandId { get; set; }
+        string Brand { get; set; }
+
+        int ModelId { get; set; }
+        string Model { get; set; }
+
+		int GenerationId { get; set; }
+		string Generation { get; set; }
+
+        int BodyTypeId { get; set; }
+        string BodyType { get; set; }
+
+        int EngineId { get; set; }
+        string Engine { get; set; }
+
+        int DriveTypeId { get; set; }
+        string DriveType { get; set; }
+
+        int TransmissionId { get; set; }
+        string Transmission { get; set; }
+
+        void Save();
+        void Delete();
+    }
+}
