7dc7f46eaa55192ac2149961f2ddc2ca4acfbc69
Euheni <eugene.vezhnavets@gmail>
2023-03-30 16:31:41 +0300

    Add to DIConfiguration AccountManager reference; add to DiConfigurator identity Configurator and fix it

--- src/DiConfigurator/DiConfigurator.cs
+++ src/DiConfigurator/DiConfigurator.cs
@@ -1,9 +1,11 @@
-﻿using eAuto.Data;
+﻿using AccountManager;
+using eAuto.Data;
 using eAuto.Data.Context;
 using eAuto.Data.Interfaces;
 using eAuto.Domain.Interfaces;
 using eAuto.Domain.Services;
 using eAuto.Storage;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -11,11 +13,15 @@ namespace DiConfiguration
 {
     public sealed class DiConfigurator
     {
-        private readonly string _connectionString;
+        private readonly string _identityConnection;
+        private readonly string _appDbConnection;
+        private readonly IConfiguration _configuration;
 
-		public DiConfigurator(string connectionString)
+		public DiConfigurator(string identityConnection, string appDbConnection, IConfiguration configuration)
         {
-            _connectionString = connectionString;
+            _identityConnection = identityConnection;
+            _appDbConnection = appDbConnection;
+            _configuration = configuration;
         }
 
         public void ConfigureServices(IServiceCollection services, ILoggingBuilder loggingBuilder)
@@ -26,6 +32,8 @@ namespace DiConfiguration
 
         private void RegisterBuisnessPart(IServiceCollection services)
         {
+            IdentityConfigurator.Configure(services, _identityConnection);
+
             services.AddTransient<IBodyTypeService, BodyTypeService>();
             services.AddTransient<IBrandService, BrandService>();
             services.AddTransient<IModelService, ModelService>();
@@ -38,8 +46,8 @@ namespace DiConfiguration
 
         private void RegisterDataPart(IServiceCollection services)
         {
-            services.AddTransient(s => new EAutoContext(_connectionString));
-            EAutoContextConfigurator.RegisterContext(services, _connectionString);
+            services.AddTransient(s => new EAutoContext(_appDbConnection));
+            EAutoContextConfigurator.RegisterContext(services, _appDbConnection);
 
             services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
             services.AddSingleton(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
