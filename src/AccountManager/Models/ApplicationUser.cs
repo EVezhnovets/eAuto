5f2933e5924f00494adc7c0126fed7aa8ac9a766
Euheni <eugene.vezhnavets@gmail>
2023-03-30 16:23:49 +0300

    Add IApplicationUser to AccountManager.Interfaces, ApplicationUser to AccountManager

---
+++ src/AccountManager/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using AccountManager.Interfaces.Models;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AccountManager.Models
+{
+	public class ApplicationUser : IdentityUser, IApplicationUser
+	{
+		[Required]
+		public string FirstName { get; set; }
+		[Required]
+		public string LastName { get; set; }
+		[Required]
+		public string Phone { get; set; }
+	}
+}
