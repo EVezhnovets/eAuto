3b04e6d6beb87546b828568d4e48fef0074af42c
Euheni <eugene.vezhnavets@gmail>
2023-04-03 14:04:47 +0300

    Add filters to Admin Cars index view; fix tables styles in admin crud views, css

--- src/eAuto.Web/Areas/Admin/Controllers/CarController.cs
+++ src/eAuto.Web/Areas/Admin/Controllers/CarController.cs
@@ -50,7 +50,7 @@ namespace eAuto.Web.Areas.Admin.Controllers
 		}
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(CarsIndexViewModel carsIndex, int? pageId)
         {
 			try
 			{
@@ -80,8 +80,46 @@ namespace eAuto.Web.Areas.Admin.Controllers
                         TransmissionId = i.TransmissionId,
                         Transmission = i.Transmission
 					});
- 
-                return View(carsList);
+
+				var carsQuery = carsList
+					.Where(i => (!carsIndex.BrandFilterApplied.HasValue || i.BrandId == carsIndex.BrandFilterApplied)
+								&& (!carsIndex.ModelFilterApplied.HasValue || i.ModelId == carsIndex.ModelFilterApplied)
+								&& (!carsIndex.GenerationFilterApplied.HasValue || i.GenerationId == carsIndex.GenerationFilterApplied)
+								&& (!carsIndex.BodyTypeFilterApplied.HasValue || i.BodyTypeId == carsIndex.BodyTypeFilterApplied)
+								&& (!carsIndex.EngineFilterApplied.HasValue || i.EngineId == carsIndex.EngineFilterApplied)
+								&& (!carsIndex.DriveTypeFilterApplied.HasValue || i.DriveTypeId == carsIndex.DriveTypeFilterApplied)
+								&& (!carsIndex.TransmissionFilterApplied.HasValue || i.TransmissionId == carsIndex.TransmissionFilterApplied))
+					.Select(i =>
+						new CarViewModel(
+							i.CarId,
+							i.PriceInitial,
+							i.PictureUrl,
+							i.Year,
+							i.DateArrival,
+							i.Odometer,
+							i.Description,
+							i.Brand,
+							i.Model,
+							i.Generation,
+							i.BodyType,
+							i.Engine,
+							i.DriveType,
+							i.Transmission
+					)).ToList();
+
+				var carsResult = new CarsIndexViewModel()
+				{
+					CarVModels = carsQuery,
+					Brands = (await GetBrands()).ToList(),
+					Models = (await GetModels()).ToList(),
+					Generations = (await GetGenerations()).ToList(),
+					BodyTypes = (await GetBodyTypes()).ToList(),
+					Engines = (await GetEngines()).ToList(),
+					DriveTypes = (await GetDriveTypes()).ToList(),
+					Transmissions = (await GetTransmissions()).ToList(),
+
+				};
+				return View(carsResult);
 			}
 			catch (CarNotFoundException ex)
 			{
@@ -386,5 +424,129 @@ namespace eAuto.Web.Areas.Admin.Controllers
             }
             #endregion
         }
-    }
+
+		private async Task<IEnumerable<SelectListItem>> GetBrands()
+		{
+			var brands = await _brandService.GetBrandModelsAsync();
+			IEnumerable<BrandViewModel> brandsVM = brands.Select(i => new BrandViewModel(i.BrandId, i.Name));
+
+			var items = brandsVM
+				.Select(brand => new SelectListItem() { Value = brand.BrandId.ToString(), Text = brand.Name })
+				.OrderBy(brand => brand.Text)
+				.ToList();
+
+			var allItems = new SelectListItem() { Value = null, Text = "All Brands", Selected = true };
+			items.Insert(0, allItems);
+
+			return items;
+		}
+
+		private async Task<IEnumerable<SelectListItem>> GetModels()
+		{
+			var models = await _modelService.GetModelModelsAsync();
+			IEnumerable<ModelViewModel> modelsVM = models.Select(i => new ModelViewModel(i.ModelId, i.Name, i.Brand));
+
+			var items = modelsVM
+				.Select(model => new SelectListItem { Value = model.ModelId.ToString(), Text = model.Name })
+				.OrderBy(model => model.Text)
+				.ToList();
+
+			var allItems = new SelectListItem() { Value = null, Text = "All Models", Selected = true };
+			items.Insert(0, allItems);
+
+			return items;
+		}
+
+		private async Task<IEnumerable<SelectListItem>> GetGenerations()
+		{
+			var generations = await _generationService.GetGenerationModelsAsync();
+			IEnumerable<GenerationViewModel> generationsVM = generations.Select(i => new GenerationViewModel(i.GenerationId, i.Name, i.Brand, i.Model));
+
+			var items = generationsVM
+				.Select(generation => new SelectListItem { Value = generation.GenerationId.ToString(), Text = generation.Name })
+				.OrderBy(generation => generation.Text)
+				.ToList();
+
+			var allItems = new SelectListItem() { Value = null, Text = "All Generations", Selected = true };
+			items.Insert(0, allItems);
+
+			return items;
+		}
+
+		private async Task<IEnumerable<SelectListItem>> GetBodyTypes()
+		{
+			var bodyTypes = await _bodyTypeService.GetBodyTypeModelsAsync();
+			IEnumerable<BodyTypeViewModel> bodyTypesVM = bodyTypes.Select(i => new BodyTypeViewModel(i.BodyTypeId, i.Name));
+
+			var items = bodyTypesVM
+				.Select(bodyType => new SelectListItem { Value = bodyType.BodyTypeId.ToString(), Text = bodyType.Name })
+				.OrderBy(bodyType => bodyType.Text)
+				.ToList();
+
+			var allItems = new SelectListItem() { Value = null, Text = "All Body Types", Selected = true };
+			items.Insert(0, allItems);
+
+			return items;
+		}
+
+		private async Task<IEnumerable<SelectListItem>> GetEngines()
+		{
+			var engines = await _engineService.GetEngineModelsAsync();
+			IEnumerable<EngineViewModel> enginesVM = engines.Select(
+				i => new EngineViewModel(
+					i.EngineId,
+					i.IdentificationName,
+					i.Type,
+					i.Capacity,
+					i.Power,
+					i.Description,
+					i.Brand,
+					i.Model,
+					i.Generation));
+
+			var items = enginesVM
+				.Select(engine => new SelectListItem { Value = engine.EngineId.ToString(), Text = engine.IdentificationName })
+				.OrderBy(engine => engine.Text)
+				.ToList();
+
+			var allItems = new SelectListItem() { Value = null, Text = "All Engines", Selected = true };
+			items.Insert(0, allItems);
+
+			return items;
+		}
+
+		private async Task<IEnumerable<SelectListItem>> GetDriveTypes()
+		{
+			var driveTypes = await _driveTypeService.GetDriveTypeModelsAsync();
+			IEnumerable<DriveTypeViewModel> driveTypesVM = driveTypes.Select(
+				i => new DriveTypeViewModel(i.DriveTypeId, i.Name));
+
+			var items = driveTypesVM
+				.Select(driveType => new SelectListItem { Value = driveType.DriveTypeId.ToString(), Text = driveType.Name })
+				.OrderBy(driveType => driveType.Text)
+				.ToList();
+
+			var allItems = new SelectListItem() { Value = null, Text = "All Drive Types", Selected = true };
+			items.Insert(0, allItems);
+
+			return items;
+		}
+
+		private async Task<IEnumerable<SelectListItem>> GetTransmissions()
+		{
+			var transmissions = await _transmissionService.GetTransmissionModelsAsync();
+			IEnumerable<TransmissionViewModel> transmissionsVM = transmissions.Select(
+				i => new TransmissionViewModel(i.TransmissionId, i.Name));
+
+			var items = transmissionsVM
+				.Select(transmission => new SelectListItem { Value = transmission.TransmissionId.ToString(), Text = transmission.Name })
+				.OrderBy(transmission => transmission.Text)
+				.ToList();
+
+			var allItems = new SelectListItem() { Value = null, Text = "All Transmissions", Selected = true };
+			items.Insert(0, allItems);
+
+			return items;
+		}
+	}
 }
