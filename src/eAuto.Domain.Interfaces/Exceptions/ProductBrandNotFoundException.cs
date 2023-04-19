3763b905317dce7b886bc7e7290460285a6433ea
Euheni <eugene.vezhnavets@gmail>
2023-04-19 16:09:50 +0300

    Add ProductBrandDomainModel, service with interfaces, excpetions

---
+++ src/eAuto.Domain.Interfaces/Exceptions/ProductBrandNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace eAuto.Domain.Interfaces.Exceptions
+{
+    public class ProductBrandNotFoundException : Exception
+    {
+        public ProductBrandNotFoundException() { }
+        public ProductBrandNotFoundException(string message) : base(message) { }
+    }
+}
---
+++ src/eAuto.Domain/Services/ProductBrandService.cs
@@ -0,0 +1,78 @@
+﻿using eAuto.Data.Interfaces;
+using eAuto.Data.Interfaces.DataModels;
+using eAuto.Domain.DomainModels;
+using eAuto.Domain.Interfaces;
+using eAuto.Domain.Interfaces.Exceptions;
+
+namespace eAuto.Domain.Services
+{
+    public sealed class ProductBrandService : IProductBrandService
+	{
+        private readonly IRepository<ProductBrandDataModel> _productBrandRepository;
+        private readonly IAppLogger<ProductBrandService> _logger;
+
+        public ProductBrandService(
+            IRepository<ProductBrandDataModel> productBrandRepository,
+            IAppLogger<ProductBrandService> logger)
+        {
+            _productBrandRepository = productBrandRepository;
+            _logger = logger;
+        }
+
+        public IProductBrand GetProductBrandModel(int id)
+        {
+            var productBrandDataModel = GetProductBrand(id);
+
+            if (productBrandDataModel == null)
+            {
+				var exception = new ProductBrandNotFoundException("ProductBrand not found");
+				_logger.LogError(exception, exception.Message);
+			}
+
+            var productBrandViewModel = 
+                new ProductBrandDomainModel(productBrandDataModel, _productBrandRepository);
+            return productBrandViewModel;
+        }
+
+        public async Task<IEnumerable<IProductBrand>> GetProductBrandModelsAsync()
+        {
+            var productBrandEntities = await _productBrandRepository.GetAllAsync();
+
+            if (productBrandEntities == null)
+            {
+				var exception = new ProductBrandNotFoundException("ProductBrand not found");
+				_logger.LogError(exception, exception.Message);
+			}
+
+            var productBrandViewModels = productBrandEntities
+                .Select(i => new ProductBrandDomainModel()
+                {
+                    ProductBrandId = i.ProductBrandDataModelId,
+                    Name = i.Name,
+                }).ToList();
+            var productBrandModels = productBrandViewModels.Cast<IProductBrand>();
+            return productBrandModels;
+        }
+ 
+        public IProductBrand CreateProductBrandDomainModel()
+        {
+            var productBrand = new ProductBrandDomainModel(_productBrandRepository);
+            return productBrand;
+        }
+
+        public ProductBrandDataModel GetProductBrand(int productBrandId)
+        {
+            var productBrand = _productBrandRepository
+                .Get(t => t.ProductBrandDataModelId == productBrandId
+                );
+
+            if (productBrand == null)
+            {
+				var exception = new ProductBrandNotFoundException("ProductBrand not found");
+				_logger.LogError(exception, exception.Message);
+			}
+
+            return productBrand;
+        }
+    }
+}
