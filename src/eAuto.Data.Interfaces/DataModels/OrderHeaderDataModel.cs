803edd9a3fdf95cd893fe41868d58c7f60d86597
Euheni <eugene.vezhnavets@gmail>
2023-04-30 16:56:41 +0300

    Add OrderHeaderDataModel, fix MotorOilDataModel, IRepository

---
+++ src/eAuto.Data.Interfaces/DataModels/OrderHeaderDataModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eAuto.Data.Interfaces.DataModels
+{
+    public class OrderHeaderDataModel
+    {
+        public class OrderHeader
+        {
+            public int Id { get; set; }
+
+            public string ApplicationUserId { get; set; }
+
+            [Required]
+            public DateTime OrderDate { get; set; }
+            [Required]
+            public DateTime ShippingDate { get; set; }
+
+            public double OrderTotal { get; set; }
+            public string? OrderStatus { get; set; }
+            public string? PaymentStatus { get; set; }
+            public string? TrackingNumber { get; set; }
+            public string? Carrier { get; set; }
+
+            public DateTime PaymentDate { get; set; }
+            public DateTime PaymentDueDate { get; set; }
+
+            public string? SessionId { get; set; }
+            public string? PaymentIntentId { get; set; }
+
+            [Required]
+            public string PhoneNumber { get; set; }
+
+            [Required]
+            public string StreetAddress { get; set; }
+
+            [Required]
+            public string City { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+        }
+    }
+}
--- src/eAuto.Data.Interfaces/DataModels/ShoppingCartDataModel.cs
+++ src/eAuto.Data.Interfaces/DataModels/ShoppingCartDataModel.cs
@@ -17,8 +17,7 @@ namespace eAuto.Data.Interfaces.DataModels
 		public int Count { get; set; }
 		public string ApplicationUserId { get; set; }
 
-		[ForeignKey("ApplicationUserId")]
-		[ValidateNever]
-		public ApplicationUser ApplicationUser { get; set; }
+		[NotMapped]
+		public double Price { get; set; }
 	}
 }
