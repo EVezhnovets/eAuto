071988b2d3be3f4aa39200dbfdeb9a3c83fa2173
Euheni <eugene.vezhnavets@gmail>
2023-05-29 22:45:21 +0300

    Add OrderDetailsDataModel, fix ApplicationUser data model, OrderHeaderDataModel, IOrderHeaderRepository

--- src/eAuto.Data.Interfaces/DataModels/OrderHeaderDataModel.cs
+++ src/eAuto.Data.Interfaces/DataModels/OrderHeaderDataModel.cs
@@ -4,40 +4,39 @@ namespace eAuto.Data.Interfaces.DataModels
 {
     public class OrderHeaderDataModel
     {
-        public class OrderHeader
-        {
-            public int Id { get; set; }
+        public int Id { get; set; }
 
-            public string ApplicationUserId { get; set; }
+        public string ApplicationUserId { get; set; }
 
-            [Required]
-            public DateTime OrderDate { get; set; }
-            [Required]
-            public DateTime ShippingDate { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
 
-            public double OrderTotal { get; set; }
-            public string? OrderStatus { get; set; }
-            public string? PaymentStatus { get; set; }
-            public string? TrackingNumber { get; set; }
-            public string? Carrier { get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        public DateTime ShippingDate { get; set; }
 
-            public DateTime PaymentDate { get; set; }
-            public DateTime PaymentDueDate { get; set; }
+        public double OrderTotal { get; set; }
+        public string? OrderStatus { get; set; }
+        public string? PaymentStatus { get; set; }
+        public string? TrackingNumber { get; set; }
+        public string? Carrier { get; set; }
 
-            public string? SessionId { get; set; }
-            public string? PaymentIntentId { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public DateTime PaymentDueDate { get; set; }
 
-            [Required]
-            public string PhoneNumber { get; set; }
+        public string? SessionId { get; set; }
+        public string? PaymentIntentId { get; set; }
 
-            [Required]
-            public string StreetAddress { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
 
-            [Required]
-            public string City { get; set; }
+        [Required]
+        public string StreetAddress { get; set; }
 
-            [Required]
-            public string Name { get; set; }
-        }
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Name { get; set; }
     }
 }
