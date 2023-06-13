e406cc21782ccfc18c1f05e66a9b5a012278f355
Euheni <eugene.vezhnavets@gmail>
2023-06-13 13:40:09 +0300

    Add session to Program.cs, to WebConstants

--- src/eAuto.Web/WebConstants.cs
+++ src/eAuto.Web/WebConstants.cs
@@ -25,5 +25,7 @@ namespace eAuto.Web
         public const string PaymentStatusApproved = "Approved";
         public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
         public const string PaymentStatusRejected = "Rejected";
+
+        public const string SessionCart = "SessionShoppingCart";
     }
 }
