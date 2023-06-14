a1e3a202583f6e23407e377af27b303771400959
Euheni <eugene.vezhnavets@gmail>
2023-06-14 11:24:39 +0300

    Add DbInitializer for Identity roles, admin user, dataConstants

---
+++ src/eAuto.Data/Identity/DbInitializer.cs
@@ -0,0 +1,49 @@
+﻿using eAuto.Data.Interfaces;
+using eAuto.Data.Interfaces.DataModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace eAuto.Data.Identity
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IdentityContext _db;
+
+        public DbInitializer(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IdentityContext db)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+        public void Initialize()
+        {
+            if (!_roleManager.RoleExistsAsync(DataConstants.AdminRole).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(DataConstants.AdminRole)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(DataConstants.CustomerRole)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(DataConstants.EmployeeRole)).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "admin@gmail.com",
+                    Email = "admin@gmail.com",
+                    FirstName = "Admin",
+                    LastName = "Admin",
+                    PhoneNumber = "+3752911122234",
+                    StreetAddress = "test 111 street",
+                    City = "Minsk"
+                },
+                   "Admin123*").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@mail.com");
+
+                _userManager.AddToRoleAsync(user, DataConstants.AdminRole).GetAwaiter().GetResult();
+            }
+            return;
+        }
+    }
+}
